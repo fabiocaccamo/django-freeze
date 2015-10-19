799eca8008bc4ee3d9daa356fde380319c20875e
Fabio Caccamo <fabio.caccamo@gmail.com>
2015-10-19 18:05:51 +0200

    Added missing package_data to setup.py and updated version to 0.1.1

--- setup.py
+++ setup.py
@@ -15,13 +15,14 @@ setup(
     author_email='fabio.caccamo@gmail.com',
     url='https://github.com/fabiocaccamo/django-freeze',
     download_url='https://github.com/fabiocaccamo/django-freeze/archive/%s.tar.gz' % __version__,
-    keywords = ['django', 'freeze', 'static', 'site', 'generator', 'generate', 'convert', 'export', 'download', 'zip'],
-    install_requires = [
+    keywords=['django', 'freeze', 'static', 'site', 'generator', 'generate', 'convert', 'export', 'download', 'zip'],
+    install_requires=[
         'Django>=1.6.5,<1.9',
         'BeautifulSoup>=3.2.1',
         'requests>=2.8.0',
         'xmltodict>=0.9.2',
     ],
+    include_package_data=True,
     classifiers=[]
 )
 
