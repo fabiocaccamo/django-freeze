7163b7b3e3f32a381cf7a9a0418221b6d09998a3
Fabio Caccamo <fabio.caccamo@gmail.com>
2015-10-22 12:27:32 +0200

    Changed default settings and some naming

--- freeze/settings.py
+++ freeze/settings.py
@@ -15,8 +15,6 @@ if not os.path.isabs(FREEZE_ROOT):
 if settings.MEDIA_ROOT.find(FREEZE_ROOT) == 0 or settings.STATIC_ROOT.find(FREEZE_ROOT) == 0:
     raise ImproperlyConfigured('settings.FREEZE_ROOT cannot be a subdirectory of MEDIA_ROOT or STATIC_ROOT')
 
-FREEZE_HTML_ROOT = os.path.abspath(os.path.normpath(settings.FREEZE_ROOT + '/html/'))
-
 FREEZE_MEDIA_ROOT = settings.MEDIA_ROOT
 FREEZE_MEDIA_URL = settings.MEDIA_URL
 
@@ -31,31 +29,35 @@ FREEZE_DOMAIN = FREEZE_SITE.domain
 FREEZE_HOST = FREEZE_PROTOCOL + FREEZE_DOMAIN
 
 FREEZE_SITEMAP_MODE = getattr(settings, 'FREEZE_SITEMAP_MODE', True)
-FREEZE_FOLLOW_MODE = getattr(settings, 'FREEZE_FOLLOW_MODE', False)
+FREEZE_FOLLOW_MODE = getattr(settings, 'FREEZE_FOLLOW_MODE', True)
 
 FREEZE_REPORT_INVALID_URLS = getattr(settings, 'FREEZE_REPORT_INVALID_URLS', False)
 FREEZE_REPORT_INVALID_URLS_SUBJECT = getattr(settings, 'FREEZE_REPORT_INVALID_URLS_SUBJECT', '[freeze] invalid urls')
 
+FREEZE_INCLUDE_MEDIA = getattr(settings, 'FREEZE_INCLUDE_MEDIA', True)
+FREEZE_INCLUDE_STATIC = getattr(settings, 'FREEZE_INCLUDE_STATIC', True)
+FREEZE_INCLUDE_STATIC_APPS = getattr(settings, 'FREEZE_INCLUDE_STATIC_APPS', ())
+FREEZE_INCLUDE_STATIC_DIRS = []
 
+if FREEZE_INCLUDE_STATIC_APPS:
+    
+    for app_name in FREEZE_INCLUDE_STATIC_APPS:
+        
+        if app_name in settings.INSTALLED_APPS:
+            
+            app_static_dir = os.path.join(FREEZE_STATIC_ROOT, app_name)
+            
+            FREEZE_INCLUDE_STATIC_DIRS.append( app_static_dir )
+        else:
+            raise ImproperlyConfigured('%s not founded in settings.INSTALLED_APPS' % (app_name, ))
+            
+
+FREEZE_ZIP_ALL = getattr(settings, 'FREEZE_ZIP_ALL', False)
 FREEZE_ZIP_NAME = getattr(settings, 'FREEZE_ZIP_NAME', 'freeze')
 
 if len(FREEZE_ZIP_NAME) >= 4 and FREEZE_ZIP_NAME[-4:].lower() != '.zip':
     FREEZE_ZIP_NAME += '.zip'
-    
+
 FREEZE_ZIP_NAME_WITH_PREFIX = '%s' + FREEZE_ZIP_NAME
-
-
 FREEZE_ZIP_PATH = os.path.abspath(os.path.join(FREEZE_ROOT, FREEZE_ZIP_NAME))
 
-FREEZE_ZIP_INCLUDE_MEDIA = getattr(settings, 'FREEZE_ZIP_INCLUDE_MEDIA', True)
-FREEZE_ZIP_INCLUDE_STATIC = getattr(settings, 'FREEZE_ZIP_INCLUDE_STATIC', True)
-FREEZE_ZIP_INCLUDE_STATIC_APPS = getattr(settings, 'FREEZE_ZIP_INCLUDE_STATIC_APPS', ())
-
-if FREEZE_ZIP_INCLUDE_STATIC_APPS:
-    
-    for app_name in FREEZE_ZIP_INCLUDE_STATIC_APPS:
-        
-        if not app_name in settings.INSTALLED_APPS:
-            raise ImproperlyConfigured('%s not founded in settings.INSTALLED_APPS' % (app_name, ))
-            
-            
