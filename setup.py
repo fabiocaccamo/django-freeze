9df9e42346d29063fc38006fa069e40b032279f9
Fabio Caccamo <fabio.caccamo@gmail.com>
2015-12-01 16:27:11 +0100

    Changed setup.py description

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ setup(
     include_package_data=True,
     license='MIT License',
     version=__version__,
-    description='django-freeze generates the static version of any site.',
+    description='django-freeze converts your django site to a static one with one line of code',
     author='Fabio Caccamo',
     author_email='fabio.caccamo@gmail.com',
     url='https://github.com/fabiocaccamo/django-freeze',
