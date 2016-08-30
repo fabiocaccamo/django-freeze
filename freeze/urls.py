9bdbbeecdf0d979b92bda9e75d0f85e66ff55b79
Fabio Caccamo <fabio.caccamo@gmail.com>
2016-08-30 11:04:32 +0200

    Fixed django.conf.urls.patterns warning

--- freeze/urls.py
+++ freeze/urls.py
@@ -1,10 +1,12 @@
 # -*- coding: utf-8 -*-
 
-from django.conf.urls import patterns, url
+from django.conf.urls import url
+
+from freeze import views
 
 
-urlpatterns = patterns('',
-    url(r'^download-static-site/$', 'freeze.views.download_static_site', name='freeze_download_static_site'), 
-    url(r'^generate-static-site/$', 'freeze.views.generate_static_site', name='freeze_generate_static_site'), 
-)
+urlpatterns = [
+    url(r'^download-static-site/$', views.download_static_site, name='freeze_download_static_site'),
+    url(r'^generate-static-site/$', views.generate_static_site, name='freeze_generate_static_site'),
+]
 
