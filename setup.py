68f1d91319dceb088dfcc147c5a43df983e1bed5
Johann Abraham <johann@arazoo.com>
2017-05-15 09:33:36 -0600

    updating FileWrapper import to use: from wsgiref.util import FileWrapper
    instead of:
    from django.core.servers.basehttp import FileWrapper
    
    as it was removed in django 1.9.
    
    In django 1.8 the basehttp.FileWrapper import pointed to
    from wsgiref.util import FileWrapper
    so this will not introduce additional issues

--- setup.py
+++ setup.py
@@ -4,6 +4,7 @@ from setuptools import setup
 
 exec(open('freeze/version.py').read())
 
+from freeze import __version__
 
 setup(
     name='django-freeze',
@@ -18,7 +19,7 @@ setup(
     download_url='https://github.com/fabiocaccamo/django-freeze/archive/%s.tar.gz' % __version__,
     keywords=['django', 'freeze', 'static', 'site', 'generator', 'generate', 'convert', 'export', 'download', 'zip'],
     install_requires=[
-        'Django>=1.6.5,<1.9',
+        'Django>=1.6.5',
         'beautifulsoup4>=4.4.1',
         'requests>=2.8.0',
         'xmltodict>=0.9.2',
