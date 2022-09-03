b9a6fbcac7f36136722b33e846f646305280156a
PV ANFAS <anfaspv.info@gmail.com>
2022-09-03 08:35:26 +0000

    Support for New Versions of Django

--- freeze/urls.py
+++ freeze/urls.py
@@ -1,12 +1,17 @@
 # -*- coding: utf-8 -*-
 
-from django.conf.urls import url
-
+from django.urls import path
 from freeze import views
 
-
 urlpatterns = [
-    url(r'^download-static-site/$', views.download_static_site, name='freeze_download_static_site'),
-    url(r'^generate-static-site/$', views.generate_static_site, name='freeze_generate_static_site'),
+    path(
+        "download-static-site/",
+        views.download_static_site,
+        name="freeze_download_static_site",
+    ),
+    path(
+        "generate-static-site/",
+        views.generate_static_site,
+        name="freeze_generate_static_site",
+    ),
 ]
-
