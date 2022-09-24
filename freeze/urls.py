26e00cf479a1b80d0ec2f249447feb62481e1c8d
PV ANFAS <45325099+pvanfas@users.noreply.github.com>
2022-09-24 14:24:06 +0400

    Update urls.py

--- freeze/urls.py
+++ freeze/urls.py
@@ -1,14 +1,14 @@
 # -*- coding: utf-8 -*-
 
 if django.VERSION < (2, 0):
-    from django.conf.urls import url as path
+    from django.conf.urls import url as re_path
 else:
-    from django.urls import path
+    from django.urls import re_path
 
 from freeze import views
 
 
 urlpatterns = [
-    path("download-static-site/", views.download_static_site, name="freeze_download_static_site"),
-    path("generate-static-site/", views.generate_static_site, name="freeze_generate_static_site"),
+    re_path(r'^download-static-site/$', views.download_static_site, name='freeze_download_static_site'),
+    re_path(r'^generate-static-site/$', views.generate_static_site, name='freeze_generate_static_site'),
 ]
