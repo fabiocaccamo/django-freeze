77230ead56191a3f3055ac4bb13cd5615339172b
Fabio Caccamo <fabio.caccamo@gmail.com>
2022-09-25 15:08:44 +0200

    Add metadata module.

---
+++ freeze/metadata.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+
+__author__ = "Fabio Caccamo"
+__copyright__ = "Copyright (c) 2016 Fabio Caccamo"
+__description__ = (
+    "convert your dynamic django site to a static one with one line of code."
+)
+__email__ = "fabio.caccamo@gmail.com"
+__license__ = "MIT"
+__title__ = "django-freeze"
+__version__ = "0.7.0"
