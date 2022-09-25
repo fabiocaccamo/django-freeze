1a3417736906edc67cb60289e4689feed836d8ee
Fabio Caccamo <fabio.caccamo@gmail.com>
2022-09-25 15:09:31 +0200

    Update setup.

--- setup.py
+++ setup.py
@@ -1,38 +1,97 @@
-
 #!/usr/bin/env python
-from setuptools import setup
+# -*- coding: utf-8 -*-
 
-exec(open('freeze/version.py').read())
+from setuptools import find_packages, setup
 
-from freeze import __version__
+import os, sys
+
+exec(open("freeze/metadata.py").read())
+
+github_url = "https://github.com/fabiocaccamo"
+sponsor_url = "https://github.com/sponsors/fabiocaccamo/"
+twitter_url = "https://twitter.com/fabiocaccamo"
+package_name = "django-freeze"
+package_url = "{}/{}".format(github_url, package_name)
+package_path = os.path.abspath(os.path.dirname(__file__))
+long_description_file_path = os.path.join(package_path, "README.md")
+long_description_content_type = "text/markdown"
+long_description = ""
+try:
+    long_description_file_options = (
+        {} if sys.version_info[0] < 3 else {"encoding": "utf-8"}
+    )
+    with open(long_description_file_path, "r", **long_description_file_options) as f:
+        long_description = f.read()
+except IOError:
+    pass
 
 setup(
-    name='django-freeze',
-    packages=['freeze', 'freeze.management', 'freeze.management.commands'],
+    name=package_name,
+    packages=find_packages(exclude=["contrib", "docs", "tests*"]),
     include_package_data=True,
-    license='MIT License',
     version=__version__,
-    description='django-freeze converts your django site to a static one with one line of code',
-    author='Fabio Caccamo',
-    author_email='fabio.caccamo@gmail.com',
-    url='https://github.com/fabiocaccamo/django-freeze',
-    download_url='https://github.com/fabiocaccamo/django-freeze/archive/%s.tar.gz' % __version__,
-    keywords=['django', 'freeze', 'static', 'site', 'generator', 'generate', 'convert', 'export', 'download', 'zip'],
+    description=__description__,
+    long_description=long_description,
+    long_description_content_type=long_description_content_type,
+    author=__author__,
+    author_email=__email__,
+    url=package_url,
+    download_url="{}/archive/{}.tar.gz".format(package_url, __version__),
+    project_urls={
+        "Documentation": "{}#readme".format(package_url),
+        "Issues": "{}/issues".format(package_url),
+        "Funding": sponsor_url,
+        "Twitter": twitter_url,
+    },
+    keywords=[
+        "django",
+        "freeze",
+        "static",
+        "site",
+        "generator",
+        "generate",
+        "convert",
+        "export",
+        "download",
+        "zip",
+    ],
     install_requires=[
-        'Django>=1.6.5',
-        'beautifulsoup4>=4.4.1',
-        'requests>=2.8.0',
-        'xmltodict>=0.9.2',
+        "django >= 1.8",
+        "beautifulsoup4 >= 4.4.1",
+        "requests >= 2.8.0",
+        "xmltodict >= 0.9.2",
     ],
     classifiers=[
-        'Framework :: Django',
-        'License :: OSI Approved :: MIT License',
-        'Programming Language :: Python :: 2',
-        'Programming Language :: Python :: 2.6',
-        'Programming Language :: Python :: 2.7',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.5',
-        'Programming Language :: Python',
+        "Development Status :: 5 - Production/Stable",
+        "Environment :: Web Environment",
+        "Framework :: Django",
+        "Framework :: Django :: 1.8",
+        "Framework :: Django :: 1.9",
+        "Framework :: Django :: 1.10",
+        "Framework :: Django :: 1.11",
+        "Framework :: Django :: 2.0",
+        "Framework :: Django :: 2.1",
+        "Framework :: Django :: 2.2",
+        "Framework :: Django :: 3.0",
+        "Framework :: Django :: 3.1",
+        "Framework :: Django :: 3.2",
+        "Framework :: Django :: 4.0",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Natural Language :: English",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 2",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.4",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Topic :: Software Development :: Build Tools",
     ],
+    license=__license__,
+    test_suite="runtests.runtests",
 )
-
