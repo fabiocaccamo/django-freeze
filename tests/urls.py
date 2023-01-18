76030cb556dd4e2872af9c8a6e3cf6a9011041f7
Fabio Caccamo <fabio.caccamo@gmail.com>
2023-01-18 09:43:44 +0100

    Run `django-upgrade` `pre-commit` hook.

--- tests/urls.py
+++ tests/urls.py
@@ -1,15 +1,15 @@
-from django.urls import include, re_path
+from django.urls import include, path
 
 from freeze.views import download_static_site, generate_static_site
 
 urlpatterns = [
-    re_path(
-        r"^download-static-site/$",
+    path(
+        "download-static-site/",
         download_static_site,
         name="freeze_download_static_site",
     ),
-    re_path(
-        r"^generate-static-site/$",
+    path(
+        "generate-static-site/",
         generate_static_site,
         name="freeze_generate_static_site",
     ),
