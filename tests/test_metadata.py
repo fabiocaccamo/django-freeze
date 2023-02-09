62716f59f1d6a393e5c5954d26959e9b5747f971
Fabio Caccamo <fabio.caccamo@gmail.com>
2023-02-09 19:10:47 +0100

    Add `metadata` module and read package attrs dynamically.

--- tests/test_metadata.py
+++ tests/test_metadata.py
@@ -1,4 +1,16 @@
+import re
+
 from django.test import TestCase
+
+from freeze.metadata import (
+    __author__,
+    __copyright__,
+    __description__,
+    __email__,
+    __license__,
+    __title__,
+    __version__,
+)
 
 
 class MetadataTestCase(TestCase):
@@ -7,12 +19,16 @@ class MetadataTestCase(TestCase):
     """
 
     def test_metadata(self):
-        from freeze.metadata import (
-            __author__,
-            __copyright__,
-            __description__,
-            __email__,
-            __license__,
-            __title__,
-            __version__,
-        )
+        self.assertTrue(isinstance(__author__, str))
+        self.assertTrue(isinstance(__copyright__, str))
+        self.assertTrue(isinstance(__description__, str))
+        self.assertTrue(isinstance(__email__, str))
+        self.assertTrue(isinstance(__license__, str))
+        self.assertTrue(isinstance(__title__, str))
+        self.assertTrue(isinstance(__version__, str))
+
+    def test_version(self):
+        v = __version__
+        v_re = re.compile(r"^([0-9]+)(\.([0-9]+)){1,2}$")
+        v_match = v_re.match(v)
+        self.assertTrue(v_match is not None)
